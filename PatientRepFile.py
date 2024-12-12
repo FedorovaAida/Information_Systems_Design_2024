ffa7518c76fd52a38e22e029d4d415eac03c9dc6
FedorovaAida <112868534+FedorovaAida@users.noreply.github.com>
2024-12-12 19:30:34 +0300

    Lab. 2.6 Update class ClientRepFile
    Изменение структуры класса, для использования адаптера

--- PatientRepFile.py
+++ PatientRepFile.py
@@ -1,22 +1,24 @@
 import os
 from PatientRepFileStrategy import PatientRepFileStrategy
 
+
 # Класс, который использует стратегию для работы с файлами
 class PatientRepFile:
-    
+
     def __init__(self, strategy: PatientRepFileStrategy):
-        self.strategy = strategy
-    
-    def get_all(self):
-        """Получить все элементы"""
-        return self.strategy.read()
-    
-    def add_entity(self, first_name, last_name, email, gender, phone, disease):      
+        self._data = []
+        self._strategy = strategy
+
+    def write_data_to_file(self):
+        self._strategy.write(self._data)
+
+    def read_data_from_file(self):
+        self._data = self._strategy.read()
+
+    def add_entity(self, first_name, last_name, email, gender, phone, disease):
         """Добавить нового пациента в список с новым ID"""
-        # Чтение данных из файла
-        data = self.strategy.read()
         # Генерация нового ID
-        new_id = max([entry['id'] for entry in data], default=0) + 1   
+        new_id = max([entry['id'] for entry in self._data], default=0) + 1
         new_entity = {
             'id': new_id,
             'first_name': first_name,
@@ -28,43 +30,37 @@ class PatientRepFile:
             'disease': disease
         }
         # Проверка на уникальность почты
-        if any(entry['email'] == email for entry in data):
-            raise ValueError('Email должен быть уникальным!')       
+        if any(entry['email'] == email for entry in self._data):
+            raise ValueError('Email должен быть уникальным!')
         # Добавляем нового пациента в список
-        data.append(new_entity)      
-        # Записываем обновленные данные в файл
-        self.strategy.write(data)
+        self._data.append(new_entity)
 
     def get_by_id(self, patient_id):
         """Получить пациента по ID"""
-        data = self.strategy.read()
-        for entry in data:
+        for entry in self._data:
             if entry['id'] == id:
                 return entry
         return None  # Если объект не найден
 
     def get_k_n_short_list(self, k, n):
         """Получить список k по счету n объектов"""
-        data = self.strategy.read()
         start = (n - 1) * k
         end = start + k
-        return data[start:end]
+        return self._data[start:end]
 
     def sort_by_field(self, field):
         """Сортировать элементы по выбранному полю"""
-        data = self.strategy.read()
         if field in ["first_name", "last_name", "email"]:
-            data.sort(key=lambda x: x.get(field))
-        return data
+            self._data.sort(key=lambda x: x.get(field))
+        return self._data
 
     def replace_by_id(self, entity_id, first_name, last_name, email, gender, phone, disease):
         """Заменить данные пациента по ID"""
-        data = self.strategy.read()
         entity = self.get_by_id(entity_id)
         if not entity:
             raise ValueError(f"Пациент с ID {entity_id} не найден.")
         # Проверка на уникальность почты
-        if email and email != entity['email'] and any(entry['email'] == email for entry in data):
+        if email and email != entity['email'] and any(entry['email'] == email for entry in self._data):
             raise ValueError('Email должен быть уникальным!')
         # Обновляем данные
         if first_name:
@@ -79,19 +75,19 @@ class PatientRepFile:
             entity['phone'] = phone
         if disease:
             entity['disease'] = disease
-        # Записываем обновленные данные в файл
-        self.strategy.write(data)
 
     def delete_by_id(self, entity_id):
         """Удалить пациента по ID"""
-        data = self.strategy.read()
         entity = self.get_by_id(entity_id)
         if not entity:
             raise ValueError(f"Пациент с ID {entity_id} не найден.")
-        data.remove(entity)
-        self.strategy.write(data)
+        self._data.remove(entity)
 
     def get_count(self):
-            """Получить количество элементов"""
-            data = self.strategy.read()
-            return len(data)
+        """Получить количество элементов"""
+        return len(self._data)
+    
+    def get_count(self):
+        """Получить количество элементов"""
+        data = self.strategy.read()
+        return len(data)
