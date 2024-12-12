a1bdd7cdb9bcfe7a17409b7dda11de4968369ae3
FedorovaAida <112868534+FedorovaAida@users.noreply.github.com>
2024-12-12 19:37:25 +0300

    Lab. 2.6 Changing names of functions
    Изменение наименований функций

--- PatientRepFileAdapter.py
+++ PatientRepFileAdapter.py
@@ -1,28 +1,28 @@
 import PatientRepFile
 
 class PatientRepFileAdapter:
-    
+
     def __init__(self, patient_rep_file: PatientRepFile):
         patient_rep_file.read_data_from_file()
         self._patient_rep_file = patient_rep_file
-    
+
     def get_k_n_short_list(self, k, n):
         return self._patient_rep_file.get_k_n_short_list(k, n)
-    
+
     def get_by_id(self, id):
         return self._patient_rep_file.get_by_id(id)
-    
+
     def delete_by_id(self, id):
         self._patient_rep_file.delete_by_id(id)
         self._patient_rep_file.write_data_to_file()
-    
-    def replace_by_id(self, entity_id, first_name, last_name, email, gender, phone, disease):
+
+    def update_by_id(self, entity_id, first_name, last_name, email, gender, phone, disease):
         self._patient_rep_file.replace_by_id(entity_id, first_name, last_name, email, gender, phone, disease)
         self._patient_rep_file.write_data_to_file()
-    
-    def add_entity(self, first_name, last_name, email, gender, phone, disease):
+
+    def add(self, first_name, last_name, email, gender, phone, disease):
         self._patient_rep_file.add_entity(first_name, last_name, email, gender, phone, disease)
         self._patient_rep_file.write_data_to_file()
-    
+
     def get_count(self):
         return self._patient_rep_file.get_count()
