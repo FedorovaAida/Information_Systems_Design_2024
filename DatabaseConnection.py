07067ace46c71c424dbd4831861d70bc32b20d4a
FedorovaAida <112868534+FedorovaAida@users.noreply.github.com>
2024-12-13 22:03:41 +0300

    Lab 2.2 Making the mail field unique
    Реализация поля почта уникальным

--- DatabaseConnection.py
+++ DatabaseConnection.py
@@ -1,15 +1,15 @@
 import psycopg2
 class DatabaseConnection:
-    
+
     """Класс для управления подключением к базе данных (Singleton)."""
     _instance = None
-    
+
     def __new__(cls, db_config):
         if cls._instance is None:
             cls._instance = super(DatabaseConnection, cls).__new__(cls)
             cls._instance._initialize(db_config)
         return cls._instance
-    
+
     def _initialize(self, db_config):
         self.connection = psycopg2.connect(
             dbname=db_config['dbname'],
@@ -20,10 +20,10 @@ class DatabaseConnection:
         )
         self.connection.autocommit = True
         self.ensure_table_exists()
-    
+
     def get_cursor(self):
         return self.connection.cursor()
-    
+
     def table_exists(self, table_name):
         """Проверяет, существует ли таблица в базе данных."""
         with self.get_cursor() as cursor:
@@ -36,7 +36,7 @@ class DatabaseConnection:
             """, (table_name,))
             result = cursor.fetchone()
         return result[0]
-    
+
     def ensure_table_exists(self):
         """Убеждается, что таблица patient существует, и создает её при необходимости."""
         if not self.table_exists("patient"):
@@ -45,11 +45,11 @@ class DatabaseConnection:
                     CREATE TABLE patient (
                         id UUID PRIMARY KEY,
                         first_name VARCHAR(50) NOT NULL,
-                        last_name VARCHAR(50) NOT NULL,
-                        email VARCHAR(255) NOT NULL,
-                        gender VARCHAR(1) CHECK (male IN ('м', 'ж')),
+                        last_name VARCHAR(50) NOT NULL,                       
+                        gender VARCHAR(1) CHECK (gender IN ('м', 'ж')),
                         phone VARCHAR(15) NOT NULL,
-                        disease VARCHAR(500) NOT NULL
+                        disease VARCHAR(500) NOT NULL,
+                        email VARCHAR(255) UNIQUE
                     );
                 """)
                 print("Таблица 'patient' успешно создана.")
