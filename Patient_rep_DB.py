05095061a37e92fed9ac42835cf54ef681f9e733
FedorovaAida <112868534+FedorovaAida@users.noreply.github.com>
2024-12-16 21:10:37 +0300

    Lab. 3.2 Update methods, that get and write data
    Исправление методов получения и записи данных в БД

--- Patient_rep_DB.py
+++ Patient_rep_DB.py
@@ -1,5 +1,7 @@
 import uuid
+import psycopg2.errors
 from DatabaseConnection import DatabaseConnection
+from Patient import Patient
 
 class PatientRepDB:
     """Класс для управления сущностью patient."""
@@ -12,27 +14,50 @@ class PatientRepDB:
         with self.db.get_cursor() as cursor:
             cursor.execute("SELECT * FROM patient WHERE id = %s", (patient_id,))
             result = cursor.fetchone()
-        return result
+        if result:
+            return Patient(
+                id=result['id'],
+                first_name=result['first_name'],
+                last_name=result['last_name'],
+                email=result['email'],
+                gender=result['gender'],
+                phone=result['phone'],
+                disease=result['disease'])
+        return None
 
     def get_k_n_short_list(self, k, n):
         """Получить список из k элементов, начиная с n-го блока."""
         with self.db.get_cursor() as cursor:
-            offset = k * (n - 1)
             cursor.execute("""
-                SELECT id, first_name, last_name, phone FROM patient
-                ORDER BY id LIMIT %s OFFSET %s
-            """, (k, offset))
+                SELECT id, first_name, last_name, email, gender, phone, disease FROM patient
+                ORDER BY first_name, last_name, id LIMIT %s OFFSET %s
+            """, (k, n))
+
             result = cursor.fetchall()
-        return result
+        return [
+            Patient(
+                id=row['id'],
+                first_name=row['first_name'],
+                last_name=row['last_name'],
+                email=row['email'],
+                gender=row['gender'],
+                phone=row['phone'],
+                disease=row['disease']
+            ) for row in result
+        ]
 
     def add(self, patient):
         """Добавить нового пациента."""
         new_id = str(uuid.uuid4())
         with self.db.get_cursor() as cursor:
-            cursor.execute("""
-                INSERT INTO patient (id, first_name, last_name, email, gender, phone, disease)
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-            """, (new_id, patient.get_first_name(), patient.get_last_name(), patient.get_email(), patient.get_gender(), patient.get_phone(), patient.get_disease()))
+            try:
+                cursor.execute("""
+                    INSERT INTO patient (id, first_name, last_name, email, gender, phone, disease)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                """, (new_id, patient.get_first_name(), patient.get_last_name(), patient.get_email(), patient.get_gender(), patient.get_phone(), patient.get_disease())) #first_name, last_name, email, gender, phone, disease))
+
+            except psycopg2.errors.UniqueViolation:
+                raise ValueError('Email должен быть уникальным!')
         return new_id
 
     def update_by_id(self, patient):
@@ -52,7 +77,7 @@ class PatientRepDB:
         fields.append("disease = %s")
         values.append(patient.get_disease())
         values.append(patient.get_id())
-       
+
         with self.db.get_cursor() as cursor:
             cursor.execute(f"""
                 UPDATE patient
