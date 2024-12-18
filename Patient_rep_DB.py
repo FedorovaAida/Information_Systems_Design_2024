3b060d0e6dcc5ffbc491f98580e4675d8ae6c5a1
FedorovaAida <112868534+FedorovaAida@users.noreply.github.com>
2024-12-18 04:08:49 +0300

    Lab 3 Changing the logic of work with the addition of the Patient class
    Изменение логики работы с добавлением класса Patient

--- Patient_rep_DB.py
+++ Patient_rep_DB.py
@@ -1,32 +1,26 @@
 import uuid
 import psycopg2.errors
 from DatabaseConnection import DatabaseConnection
-from Patient import Patient
+
 
 class PatientRepDB:
-    """Класс для управления сущностью patient."""
+
     def __init__(self, db_config):
         self.db = DatabaseConnection(db_config)
 
-
     def get_by_id(self, patient_id):
-        """Получить пациента по ID."""
+        from Patient import Patient
         with self.db.get_cursor() as cursor:
             cursor.execute("SELECT * FROM patient WHERE id = %s", (patient_id,))
             result = cursor.fetchone()
         if result:
-            return Patient(
-                id=result['id'],
-                first_name=result['first_name'],
-                last_name=result['last_name'],
-                email=result['email'],
-                gender=result['gender'],
-                phone=result['phone'],
-                disease=result['disease'])
+            columns_names = [desc[0] for desc in cursor.description]
+            patient = Patient(**dict(zip(columns_names, result)))
+            return patient
         return None
 
     def get_k_n_short_list(self, k, n):
-        """Получить список из k элементов, начиная с n-го блока."""
+        from Patient import Patient
         with self.db.get_cursor() as cursor:
             cursor.execute("""
                 SELECT id, first_name, last_name, email, gender, phone, disease FROM patient
@@ -34,34 +28,31 @@ class PatientRepDB:
             """, (k, n))
 
             result = cursor.fetchall()
-        return [
-            Patient(
-                id=row['id'],
-                first_name=row['first_name'],
-                last_name=row['last_name'],
-                email=row['email'],
-                gender=row['gender'],
-                phone=row['phone'],
-                disease=row['disease']
-            ) for row in result
+        columns_names = [desc[0] for desc in cursor.description]
+        return[
+            Patient(**dict(zip(columns_names, row)))
+            for row in result
         ]
 
     def add(self, patient):
-        """Добавить нового пациента."""
         new_id = str(uuid.uuid4())
         with self.db.get_cursor() as cursor:
             try:
                 cursor.execute("""
                     INSERT INTO patient (id, first_name, last_name, email, gender, phone, disease)
-                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-                """, (new_id, patient.get_first_name(), patient.get_last_name(), patient.get_email(), patient.get_gender(), patient.get_phone(), patient.get_disease())) #first_name, last_name, email, gender, phone, disease))
-
+                    VALUES (%s, %s, %s, %s, %s, %s, %s)
+                    """, (new_id,
+                          patient.get_first_name(),
+                          patient.get_last_name(),
+                          patient.get_email(),
+                          patient.get_gender(),
+                          patient.get_phone(),
+                          patient.get_disease()))
             except psycopg2.errors.UniqueViolation:
                 raise ValueError('Email должен быть уникальным!')
         return new_id
 
     def update_by_id(self, patient):
-        """Обновить данные пациента по ID."""
         fields = []
         values = []
         fields.append("first_name = %s")
@@ -86,12 +77,10 @@ class PatientRepDB:
             """, tuple(values))
 
     def delete_by_id(self, patient_id):
-        """Удалить пациента по ID."""
         with self.db.get_cursor() as cursor:
             cursor.execute("DELETE FROM patient WHERE id = %s", (patient_id,))
 
     def get_count(self):
-        """Получить количество записей в таблице."""
         with self.db.get_cursor() as cursor:
             cursor.execute("SELECT COUNT(*) FROM patient")
             result = cursor.fetchone()
