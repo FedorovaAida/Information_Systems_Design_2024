04daa4a246c16b7c613eeacdd5e1ab16d7bcc5de
FedorovaAida <112868534+FedorovaAida@users.noreply.github.com>
2024-12-21 09:11:05 +0300

    Lab 3 Refactoring

--- PatientRepFileStrategy.py
+++ PatientRepFileStrategy.py
@@ -1,16 +1,13 @@
-import os
 from abc import ABC, abstractmethod
 
 
 # Абстрактный базовый класс стратегии работы с файлами
 class PatientRepFileStrategy(ABC):
-  
+
     @abstractmethod
     def read(self):
-        """Прочитать данные из файла"""
         pass
-      
+
     @abstractmethod
     def write(self, data):
-        """Записать данные в файл"""
         pass
