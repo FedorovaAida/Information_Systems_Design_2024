3728b823bfc83439d0a58d9d9fc9697f603a0617
FedorovaAida <112868534+FedorovaAida@users.noreply.github.com>
2024-12-21 09:10:37 +0300

    Lab 3 Refactoring

--- PatientRepFileAdapter.py
+++ PatientRepFileAdapter.py
@@ -3,7 +3,7 @@ from PatientRepFile import PatientRepFile
 class PatientRepFileAdapter:
 
     def __init__(self, patient_rep_file: PatientRepFile):
-        patient_rep_file.read_data_from_file()
+        patient_rep_file.read()
         self._patient_rep_file = patient_rep_file
 
     def get_k_n_short_list(self, k, n):
@@ -14,15 +14,15 @@ class PatientRepFileAdapter:
 
     def delete_by_id(self, id):
         self._patient_rep_file.delete_by_id(id)
-        self._patient_rep_file.write_data_to_file()
+        self._patient_rep_file.write()
 
     def update_by_id(self, patient):
         self._patient_rep_file.replace_by_id(patient)
-        self._patient_rep_file.write_data_to_file()
+        self._patient_rep_file.write()
 
     def add(self, patient):
         self._patient_rep_file.add_entity(patient)
-        self._patient_rep_file.write_data_to_file()
+        self._patient_rep_file.write()
 
     def get_count(self):
         return self._patient_rep_file.get_count()
