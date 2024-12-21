24377754ee8e8f0a9b6e8203918cc4a1c7ce2e1c
FedorovaAida <112868534+FedorovaAida@users.noreply.github.com>
2024-12-21 09:09:53 +0300

    Lab 3 Refactoring

--- PatientModel.py
+++ PatientModel.py
@@ -1,57 +1,46 @@
+# Шаблон проектирования Наблюдатель, где объект PatientModel уведомляет наблюдателей об изменениях
 class Observable:
-
-    """Класс для реализации паттерна Наблюдатель"""
+    # Класс для наблюдателей, другие классы могут подписаться на изменения в этом объекте,
+    # и быть уведомлены при их изменении
     def __init__(self):
         self._observers = []
 
     def add_observer(self, observer):
-        """Добавить наблюдателя"""
         self._observers.append(observer)
 
     def remove_observer(self, observer):
-        """Удалить наблюдателя"""
         self._observers.remove(observer)
 
     def notify_observers(self, action, data):
-        """Уведомить всех наблюдателей"""
+        # Уведомляет всех наблюдателей в списке, вызывая у них метод update,
+        # передавая тип действия action и данные data
         for observer in self._observers:
             observer.update(action, data)
 
-class PatientModel(Observable):
 
-    """Модель для управления пациентами"""
+class PatientModel(Observable):
     def __init__(self, repository):
+        # Инициализируем список наблюдателей, передается репозиторий,
+        # который отвечает за работу с бд или файлами
         super().__init__()
         self._repository = repository
 
     def get_patients(self, page_size, page_num):
-        """Получить список пациентов для отображения на странице"""
         offset = (page_num - 1) * page_size
         return self._repository.get_k_n_short_list(page_size, offset)
 
     def get_patient_by_id(self, patient_id):
-        """Получить пациента по ID"""
         return self._repository.get_by_id(patient_id)
 
-    def add_patient(self, first_name, last_name, email, gender, phone, disease):
-        """Добавить нового пациента и уведомить наблюдателей"""
-        client_id = self._repository.add(
-            first_name=first_name,
-            last_name=last_name,
-            email=email,
-            gender=gender,
-            phone=phone,
-            disease=disease
-        )
-        self.notify_observers("add", {"id": client_id, "first_name": first_name, "last_name": last_name})
+    def add_patient(self, patient):
+        patient_id = self._repository.add(patient)
+        self.notify_observers("add", {"id": patient_id, "first_name": patient.get_first_name(), "last_name": patient.get_last_name()})
 
-    def update_patient(self, patient_id, first_name, last_name, email, gender, phone, disease):
-        """Обновить данные пациента и уведомить наблюдателей"""
-        self._repository.update_by_id(patient_id, first_name, last_name, email, gender, phone, disease)
-        self.notify_observers("update", {"id": patient_id, "first_name": first_name, "last_name": last_name})
+    def update_patient(self, patient):
+        self._repository.update_by_id(patient)
+        self.notify_observers("update", {"id": patient.get_id(), "first_name": patient.get_first_name(), "last_name": patient.get_last_name()})
 
     def delete_patient(self, patient_id):
-        """Удалить пациента и уведомить наблюдателей"""
         self._repository.delete_by_id(patient_id)
         self.notify_observers("delete", {"id": patient_id})
 
